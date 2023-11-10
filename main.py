7cb7c7fa565e6eade18ab3bb7f6f6975bdb1693f
Isaiah Love <isaiahlove805@yahoo.com>
2023-11-10 15:44:20 -0800

    added streamlit to this project for front-end ui and user input

--- main.py
+++ main.py
@@ -17,87 +17,110 @@ from typing import List, Tuple, Any, Union
 # Importing specific classes from the langchain.schema module
 from langchain.schema import AgentAction, AgentFinish
 
+from langchain.callbacks import StreamlitCallbackHandler
+import streamlit as st
 
-# Load environment variables from a .env file
-load_dotenv()
-# Set API key for the OpenAI module
-openai.openai_api_key = os.getenv("OPENAI_API_KEY")
-# Set API key for the serpAPI module
-serpapi_api_key = os.getenv("SERPAPI_API_KEY")
+try:
+    # Load environment variables from a .env file
+    load_dotenv()
+    # Set API key for the OpenAI module
+    openai.openai_api_key = os.getenv("OPENAI_API_KEY")
+    # Set API key for the serpAPI module
+    serpapi_api_key = os.getenv("SERPAPI_API_KEY")
 
-# Create instances of OpenAI and ChatOpenAI
-llm = OpenAI()
-chat_model = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    # Create instances of OpenAI and ChatOpenAI
+    llm = OpenAI()
+    chat_model = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 
-# Create a function that will be used to run the OpenAI model
-search = SerpAPIWrapper(serpapi_api_key =serpapi_api_key)
-tools = [
-    Tool(
-        name="Search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events",
-        return_direct=True,
+    # Create a function that will be used to run the OpenAI model
+    search = SerpAPIWrapper(serpapi_api_key =serpapi_api_key)
+    tools = [
+        Tool(
+            name="Search",
+            func=search.run,
+            description="useful for when you need to answer questions about current events",
+            return_direct=True,
+        )
+    ]
+
+    # Defining a class named FakeAgent that inherits from BaseSingleActionAgent
+    class FakeAgent(BaseSingleActionAgent):
+        """Fake Custom Agent."""
+
+        # Defining a property named input_keys that returns a list with one element, "input"
+        @property
+        def input_keys(self):
+            return ["input"]
+
+        # Defining a method named plan that takes intermediate_steps, a list of tuples, and additional keyword arguments
+        # It returns an AgentAction object with specific properties
+        def plan(
+            self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+        ) -> Union[AgentAction, AgentFinish]:
+            """Given input, decide what to do.
+
+            Args:
+                intermediate_steps: Steps the LLM has taken to date,
+                    along with observations
+                **kwargs: User inputs.
+
+            Returns:
+                Action specifying what tool to use.
+            """
+            # Returning an AgentAction object with tool set to "Search",
+            # tool_input set to the value associated with the "input" key in kwargs, and an empty log
+            return AgentAction(tool="Search", tool_input=kwargs["input"], log="")
+
+        # Defining an asynchronous version of the plan method, named aplan
+        async def aplan(
+            self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+        ) -> Union[AgentAction, AgentFinish]:
+            """Given input, decide what to do.
+
+            Args:
+                intermediate_steps: Steps the LLM has taken to date,
+                    along with observations
+                **kwargs: User inputs.
+
+            Returns:
+                Action specifying what tool to use.
+            """
+            # Returning an AgentAction object with tool set to "Search",
+            # tool_input set to the value associated with the "input" key in kwargs, and an empty log
+            return AgentAction(tool="Search", tool_input=kwargs["input"], log="")
+
+            # Creating an instance of the FakeAgent class
+    agent = FakeAgent()
+
+    # Initializing an AgentExecutor with the FakeAgent instance, a set of tools, and verbose mode set to True
+    agent_executor = AgentExecutor.from_agent_and_tools(
+        agent=agent, tools=tools, verbose=True
     )
-]
 
-# Defining a class named FakeAgent that inherits from BaseSingleActionAgent
-class FakeAgent(BaseSingleActionAgent):
-    """Fake Custom Agent."""
+    # Running the agent executor with the input "How many people live in Canada as of 2023?"
 
-    # Defining a property named input_keys that returns a list with one element, "input"
-    @property
-    def input_keys(self):
-        return ["input"]
+      # Set the title of the Streamlit app
+    st.title("Langchain & Streamlit Demo")
+    #agent_executor.run("Who mike douglas?")
+     # Capture user input using a Streamlit chat input widget and check if there is input
+    if prompt := st.chat_input():
+        # Display the user's input as a chat message
+        st.chat_message("user").write(prompt)
 
-    # Defining a method named plan that takes intermediate_steps, a list of tuples, and additional keyword arguments
-    # It returns an AgentAction object with specific properties
-    def plan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[AgentAction, AgentFinish]:
-        """Given input, decide what to do.
+        # Display the assistant's response within a Streamlit chat message
+        with st.chat_message("assistant"):
+            # Create a new Streamlit callback handler with a container
+            st_callback = StreamlitCallbackHandler(st.container())
 
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
+            # Run the agent with the user's input and specified callbacks
+            response = agent_executor.run(prompt, callbacks=[st_callback])
 
-        Returns:
-            Action specifying what tool to use.
-        """
-        # Returning an AgentAction object with tool set to "Search",
-        # tool_input set to the value associated with the "input" key in kwargs, and an empty log
-        return AgentAction(tool="Search", tool_input=kwargs["input"], log="")
+            # Display the agent's response
+            st.write(response)
 
-    # Defining an asynchronous version of the plan method, named aplan
-    async def aplan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[AgentAction, AgentFinish]:
-        """Given input, decide what to do.
-
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
-
-        Returns:
-            Action specifying what tool to use.
-        """
-        # Returning an AgentAction object with tool set to "Search",
-        # tool_input set to the value associated with the "input" key in kwargs, and an empty log
-        return AgentAction(tool="Search", tool_input=kwargs["input"], log="")
-
-        # Creating an instance of the FakeAgent class
-agent = FakeAgent()
-
-# Initializing an AgentExecutor with the FakeAgent instance, a set of tools, and verbose mode set to True
-agent_executor = AgentExecutor.from_agent_and_tools(
-    agent=agent, tools=tools, verbose=True
-)
-
-# Running the agent executor with the input "How many people live in Canada as of 2023?"
-agent_executor.run("Who mike douglas?")
-
+except Exception as e:
+    st.error(f"An error occurred: {e}")
 
 
 
